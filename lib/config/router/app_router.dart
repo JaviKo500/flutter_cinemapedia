671eeab07fd867dc4c05878be91804165d27cf8f
JaViKo <javikogutierrez64@gmail.com>
2023-08-29 00:24:37 -0500

    state notifier

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,4 +1,4 @@
-import 'package:cinemapedia/presentacion/screens/screens.dart';
+import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
