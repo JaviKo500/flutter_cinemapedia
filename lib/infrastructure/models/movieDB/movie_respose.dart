6b1afa3d01d7b7763d6b5fa2afd618ec40d6d5ba
JaViKo <javikogutierrez64@gmail.com>
2023-08-30 23:18:16 -0500

    load movies

--- lib/infrastructure/models/movieDB/movie_respose.dart
+++ lib/infrastructure/models/movieDB/movie_respose.dart
@@ -22,7 +22,7 @@ class MovieDbResponse {
     String toRawJson() => json.encode(toJson());
 
     factory MovieDbResponse.fromJson(Map<String, dynamic> json) => MovieDbResponse(
-        dates: json["dates"] ? Dates.fromJson(json["dates"]) : null,
+        dates: json["dates"] != null ? Dates.fromJson(json["dates"]) : null,
         page: json["page"],
         results: List<MovieMovieDB>.from(json["results"].map((x) => MovieMovieDB.fromJson(x))),
         totalPages: json["total_pages"],
