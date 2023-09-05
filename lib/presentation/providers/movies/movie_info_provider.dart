b58f8bbd98e8311e1e99d9ddc3cb05092fada8eb
JaViKo <javikogutierrez64@gmail.com>
2023-09-05 05:25:52 -0500

    provider get movie

---
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -0,0 +1,22 @@
+import 'package:cinemapedia/domain_/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final movieInfoProvider =
+    StateNotifierProvider<MovieMapNotifier, Map<String, Movie>>((ref) {
+  final getMovieById = ref.watch(movieRepositoryProvider).getMovieById;
+  return MovieMapNotifier(getMovie: getMovieById);
+});
+
+typedef GetMovieCallBack = Future<Movie> Function({ required String id});
+
+class MovieMapNotifier extends StateNotifier<Map<String, Movie>> {
+  final GetMovieCallBack getMovie;
+  MovieMapNotifier({required this.getMovie}) : super({});
+
+  Future<void> loadMovie(String movieId) async {
+    if (state[movieId] != null) return;
+    final movie = await getMovie(id: movieId);
+    state = {...state, movieId: movie};
+  }
+}
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends StatefulWidget {
   static const name = 'movie-screen';
   final String movieId;
   const MovieScreen({Key? key, required this.movieId}) : super(key: key);
 
   @override
+  State<MovieScreen> createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
+
+  @override
+  void initState() {
+    
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MovieId: $movieId'),
+        title: Text('MovieId: ${widget.movieId}'),
       ),
     );
   }
