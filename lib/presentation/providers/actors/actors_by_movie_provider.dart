5ccabb17a7811801b785b52464c578c442231136
JaViKo <javikogutierrez64@gmail.com>
2023-09-16 12:58:12 -0500

    get actors

--- lib/presentation/providers/actors/actors_by_movie_provider.dart
+++ lib/presentation/providers/actors/actors_by_movie_provider.dart
@@ -1,5 +1,5 @@
 import 'package:cinemapedia/domain_/entities/actor.dart';
-import 'package:cinemapedia/presentation/providers/actos/actors_repository_provider.dart';
+import 'package:cinemapedia/presentation/providers/actors/actors_repository_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final actorsByMovieProvider =
