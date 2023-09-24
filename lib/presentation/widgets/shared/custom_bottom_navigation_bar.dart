d4f00bac3ba963aeb87276626a025b5d9cf84315
JaViKo <javikogutierrez64@gmail.com>
2023-09-23 20:47:29 -0500

    navigation bar

--- lib/presentation/widgets/shared/custom_bottom_navigation_bar.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation_bar.dart
@@ -1,28 +1,54 @@
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
+  const CustomBottomNavigationBar({Key? key}) : super(key: key);
 
-  const CustomBottomNavigationBar ({Key? key}) : super(key: key);
+  int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).matchedLocation;
+    print(location);
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  void _onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      elevation: 0,
-      items: const [
-        BottomNavigationBarItem(
-          label: 'Home',
-          icon: Icon(Icons.home_max),
-        ),
-        BottomNavigationBarItem(
-          label: 'Categories',
-          icon: Icon(Icons.label_outline),
-        ),
-        BottomNavigationBarItem(
-          label: 'Favorites',
-          icon: Icon(Icons.favorite_outline),
-        )
-      ]
-    );
+        elevation: 0,
+        currentIndex: getCurrentIndex(context),
+        onTap: (value) => _onItemTapped(context, value),
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home_max),
+          ),
+          BottomNavigationBarItem(
+            label: 'Categories',
+            icon: Icon(Icons.label_outline),
+          ),
+          BottomNavigationBarItem(
+            label: 'Favorites',
+            icon: Icon(Icons.favorite_outline),
+          )
+        ]);
   }
 }
